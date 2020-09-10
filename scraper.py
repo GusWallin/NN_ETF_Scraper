53b23566ca5b357ff9573f7ebd6c57da2e6e461a
GusWallin <gustaf.wallin@gmail.com>
2020-09-10 02:30:42 +0200

    third commit

--- scraper.py
+++ scraper.py
@@ -1,13 +1,9 @@
 """NN Scraper 
 
 Scrapes webpage for ETF information, stores in dataframs and makes simple permutations
-<<<<<<< HEAD
 before it is saved to a .csv file.
 
 good luck! 
-=======
-before it is saved to a .csv file
->>>>>>> a3a4910eaed86acbf656bed0814b0cdd62690866
 
 """
 
